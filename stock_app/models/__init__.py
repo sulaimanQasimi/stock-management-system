65390418f83aeb8778d1a6122bf9781c1bddb019
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-05-26 11:18:37 -0700

    Export stock models

--- stock_app/models/__init__.py
+++ stock_app/models/__init__.py
@@ -2,17 +2,4 @@ from .core import Category, Unit, Party, Product
 from .purchase import PurchaseBatch, PurchaseItem, PurchasePayment
 from .sale import Sale, SaleItem, SalePayment
 from .reports import StockProfitReport
-
-__all__ = [
-    'Category',
-    'Unit',
-    'Party',
-    'Product',
-    'PurchaseBatch',
-    'PurchaseItem',
-    'PurchasePayment',
-    'Sale',
-    'SaleItem',
-    'SalePayment',
-    'StockProfitReport',
-]
+from .stock import Department, StockMovement
