db494c969f6787737b514e46e2b1e5f5a053871e
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-05-27 04:36:53 -0700

    Add tenant-aware database router

---
+++ stock_management/db_router.py
@@ -0,0 +1,15 @@
+from stock_management.tenancy import get_current_database
+
+
+class TenantDatabaseRouter:
+    def db_for_read(self, model, **hints):
+        return get_current_database()
+
+    def db_for_write(self, model, **hints):
+        return get_current_database()
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        return True
