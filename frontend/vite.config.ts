13a01bf3f8376a31de3ef1fb4ed929ba7f2caae9
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-05-26 21:33:29 -0700

    Configure Vite for Inertia v3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import inertia from '@inertiajs/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [inertia(), react()],
   server: {
     port: 5173,
     proxy: {
