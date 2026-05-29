ece94d71bac4ddb09ffe436c168f78b18665ff4a
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-05-29 22:31:29 +0430

    Remove React plugin from TypeScript Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import inertia from '@inertiajs/vite';
 
 export default defineConfig({
-  plugins: [inertia(), react({ fastRefresh: false })],
+  plugins: [inertia()],
   server: {
     port: 5173,
     proxy: {
       '/api': 'http://127.0.0.1:8000'
     }
-  }
+  },
+  esbuild: {
+    jsx: 'automatic',
+  },
 });
