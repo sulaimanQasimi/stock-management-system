e7bce23eff13e5af4cf24f23db48c2767d5b16a1
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-05-30 10:48:29 +0430

    Align settings with inertia-django and django-vite

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -5,9 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-this-in-production')
-
 DEBUG = os.getenv('DJANGO_DEBUG', 'true').lower() == 'true'
-
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',') if host.strip()]
 
 INSTALLED_APPS = [
@@ -39,104 +37,47 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'stock_management.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    }
-]
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / 'templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+}]
 
 WSGI_APPLICATION = 'stock_management.wsgi.application'
 
-DB_ENGINE = os.getenv('DB_ENGINE', 'mysql').lower()
-
-if DB_ENGINE == 'sqlite':
-    default_database = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('SQLITE_NAME', BASE_DIR / 'db.sqlite3'),
-    }
-else:
-    default_database = {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stock_management',
-        'USER': 'sulaiman',
-        'PASSWORD':'YourStrongPassword123!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-
 DATABASES = {
-    'default': default_database,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-additional_databases = os.getenv('TENANT_DATABASES_JSON')
-
-if additional_databases:
-    DATABASES.update(json.loads(additional_databases))
-
-DEFAULT_TENANT_DATABASE = os.getenv('DEFAULT_TENANT_DATABASE', 'default')
-
-TENANT_DATABASE_ALIASES = [alias for alias in DATABASES.keys()]
-
-TENANT_DATABASE_LABELS = {
-    alias: alias.replace('_', ' ').title()
-    for alias in TENANT_DATABASE_ALIASES
-}
-
-DATABASE_ROUTERS = [
-    'stock_management.db_router.TenantDatabaseRouter',
-]
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = os.getenv('TIME_ZONE', 'UTC')
-
 USE_I18N = True
-
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collectstatic'
+STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'dist']
 
 INERTIA_LAYOUT = 'app.html'
 
-DJANGO_VITE_MODE=DEBUG
+DJANGO_VITE = {
+    'default': {
+        'dev_mode': DEBUG,
+    }
+}
 
-STATIC_ROOT = BASE_DIR / 'collectstatic'
-
-STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'dist']
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
