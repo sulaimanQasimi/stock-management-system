7c8e92abac76b391be61a08f42403aaa7d2c41ee
sulaimanQasimi <farzadqasimy@gmail.com>
2026-05-30 10:39:12 +0430

    Update Tailwind CSS content path to use resources directory

--- resources/tailwind.config.cjs
+++ resources/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './resources/js/**/*.{js,jsx,ts,tsx}',
     './node_modules/flowbite/**/*.js',
     './node_modules/flowbite-react/**/*.js',
   ],
